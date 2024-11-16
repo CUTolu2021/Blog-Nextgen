a95a7cd850714235bf4bc62d8d55d16697c2e0c9
Omoniyi Tolulope <omoniyitolulope05@gmail.com>
2024-11-16 19:12:19 +0100

    Done

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const blogRoute = require("./routes/blog.route")
+const userRouter = require('./routes/user.route')
+const ratingRouter = require('./routes/rating.route')
+const commentRouter = require('./routes/comment.route')
 require('dotenv').config()
 
 const app = express()
 
-mongoose.connect(process.env.MONGODB, {useNewUrlParser: true})
+app.use(express.json())
+mongoose.connect(process.env.MONGODB)
 .then(() => {
     console.log('Connected to MongoDB')
 })
 .catch(err => {
     console.log(err)
 })
-.finally(() => {
-    console.log('Connection Established')
-})
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -22,13 +25,6 @@ app.use("/blogs",blogRoute)
 app.use("/users",userRouter)
 app.use("/ratings",ratingRouter)
 app.use("/comments",commentRouter)
-
-
-//uche working
-app.put('/', (req, res) => {
-    res.send('Hello World 2024')
-})
-//latifat working
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`)
