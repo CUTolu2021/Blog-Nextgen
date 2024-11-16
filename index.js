926ac2e8f08c1d3420fe585680fdeedc013aeecb
LatifatAlarape <omotayolatifat5@gmail.com>
2024-11-16 10:58:52 +0100

    latifat work

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const express = require('express')
 const mongoose = require('mongoose')
-const blogRoute = require("./routes/blog.route")
-const userRouter = require('./routes/user.route')
-const ratingRouter = require('./routes/rating.route')
-const commentRouter = require('./routes/comment.route')
+const app = require("./app")
 require('dotenv').config()
 
-const app = express()
-
-app.use(express.json())
 mongoose.connect(process.env.MONGODB, {useNewUrlParser: true})
 .then(() => {
     console.log('Connected to MongoDB')
@@ -16,22 +9,6 @@ mongoose.connect(process.env.MONGODB, {useNewUrlParser: true})
 .catch(err => {
     console.log(err)
 })
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-app.use("/blog",blogRoute)
-app.use("/user",userRouter)
-app.use("/rating",ratingRouter)
-app.use("/comment",commentRouter)
-
-
-//uche working
-app.put('/', (req, res) => {
-    res.send('Hello World 2024')
-})
-//latifat working
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`)
